a8efb84dc225a3903aee31b581e4d10a9d6f00aa
yamamo0301 <101253886+yamamo0301@users.noreply.github.com>
2022-05-08 14:10:56 +0000

    test

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,7 +4,7 @@ class ListsController < ApplicationController
   end
   
   def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    # １.&2. データを受け取り新規登録するためのインスタンス作成!
     list = List.new(list_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
     list.save
