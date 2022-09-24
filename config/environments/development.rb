0999277366aa4f31d625616c605008279b9f16b8
yamamo0301 <yamamo0301@icloud.com>
2022-09-24 14:11:50 +0000

    [clone]rspec_practice

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,6 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  
-  config.hosts << "e7a887fa564344d9800b6172c4e93e25.vfs.cloud9.us-east-1.amazonaws.com"
+
+  config.hosts << "9c8e844a91b34fd8b101d1bd2968ecf3.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
